aba384562e91ccab37afa8b923a8f440d05c2ad2
shubham-8496 <shubham.sonawane8496@gmail.com>
2021-11-25 14:47:16 +0530

    this is first java program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//Declaring HelloWorld class.    
+public class HelloWorld{    
+    //creating main() method of the HelloWorld class    
+    public static void main(String args[]){    
+        //Printing Hello World    
+        System.out.println("This is HelloWorld! example");    
+    }    
+}
